1b69a188277631fdb01903ccbb14d13520a1664a
00arc7 <00arc7@D-DS-SF-13189>
2024-12-05 13:02:06 -0400

    remove:Code clean up

--- DefaultCoreLibrary/Application/Abstractions/Messaging/IQueryHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DefaultCoreLibrary.Application.Abstractions.Messaging;
-
-public interface IQueryHandler<TQuery, TResponse> where TQuery : IQuery<TResponse>
-{
-    Task<TResponse> Handle(TQuery query, CancellationToken cancellationToken);
-}
